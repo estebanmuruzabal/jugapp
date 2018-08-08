7a3015214e3db500d50622bc2bba0ab312dadd88
Esteban Muruzabal <emuruzabal@sweatworks.net>
2018-08-08 18:55:46 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>invita a tus amigos</Text>
+        <Text>crea un partido</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
